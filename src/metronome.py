4b505e415d8a021c173c748f205846cd8779d6ac
Johnny Brown <johnnybrown7@gmail.com>
2013-10-04 01:56:24 +0300

    add clock in function

--- src/metronome.py
+++ src/metronome.py
@@ -2,15 +2,25 @@
 
 import os
 from datetime import datetime
+import urlparse
 
 import psycopg2
 
 DB_CONN = os.environ['METRONOME_DB']
 
 def clock_in(**kwargs):
-    when = datetime.utcnow() if not kwargs.get("time_in") else when
+    kwargs['time_in'] = datetime.utcnow() if not kwargs.get("time_in") else when
 
-    conn = psycopg2.connect(DB_CONN)
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(DB_CONN)
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port)
+
     cur = conn.cursor()
 
     vals = filter(None, kwargs.items())
@@ -18,4 +28,4 @@ def clock_in(**kwargs):
     query = ("INSERT INTO entries (" + ", ".join([v[0] for v in vals]) + ")" +
              " VALUES (" + ", ".join(["%s" for v in vals]) + ")")
     cur.execute(query, tuple(v[1] for v in vals))
-    
+    conn.commit()
