16ce49b7eb54aaf25d690d498d99954daedfa7bc
Johnny Brown <jb@stafftacular.com>
2014-03-17 15:11:09 -0500

    function to compute total hours

--- src/exporters/standard.py
+++ src/exporters/standard.py
@@ -3,6 +3,8 @@ import os
 import psycopg2
 import urlparse
 
+from exporters.records import export as re
+
 DB_CONN = os.environ['METRONOME_DB']
 
 def export(client):
@@ -24,7 +26,13 @@ def export(client):
         time_in, time_out, notes = e
 
         print "\t".join([time_in.strftime("%Y-%m-%d"),
-                         str((time_out - time_in).total_seconds()/3600.0) if time_out else "",
+                         str((time_out - time_in)
+                             .total_seconds()/3600.0) if time_out else "",
                          notes if notes else "",
                          str(time_in.strftime("%c")),
                          str(time_out.strftime("%c")) if time_out else ""])
+
+def total_hours(where):
+    recs = re(where)
+    return sum((rec['time_out'] - rec['time_in']).total_seconds()
+               for rec in recs) / (60 * 60)
